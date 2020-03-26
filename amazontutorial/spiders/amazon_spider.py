f1e01ff503a6c28b696f15bf67bf7375de866a6b
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2020-03-25 20:40:00 -0700

    initial commit

---
+++ amazontutorial/spiders/amazon_spider.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import AmazontutorialItem
+
+
+class AmazonSpiderSpider(scrapy.Spider):
+    name = 'amazon'
+    start_urls = [
+        'https://www.amazon.com/Books/b?ie=UTF8&node=549028']
+
+
+    def parse(self, response):
+        items = AmazontutorialItem()
+
+        product_name = response.css('.acs-product-block__product-title .a-truncate-cut::text').extract()   
+        product_author = response.css('..acs-product-block__contributor .a-truncate-cut').css('::text').extract()   
+        product_price = response.css('..acs-product-block__price--buying span::text').css('::text').extract()   
+        product_imagelink = response.css('#twoPassCarousel img::attr(src)').extract()  
+
+        items['product_name'] = product_name 
+        items['product_author'] = product_author
+        items['product_price'] = product_price 
+        items['product_imagelink'] = product_imagelink 
+        
+        yield items
